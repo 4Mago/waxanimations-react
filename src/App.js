17bb7cedd1874cdef254d6f257387b3a4402a6bb
4Mago <47850942+4Mago@users.noreply.github.com>
2021-06-20 22:01:33 +0200

    Pushhh

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ const App = () => {
   return (
     <Cont>
       <IframeCont>
-<Iframe controls="" allow="autoplay" data-ready="true" data-vimeo-initialized="true" data-desktop="514264128" data-mobile="504532846" muted="" src="https://player.vimeo.com/video/514264128?autoplay=1&amp;loop=1&amp;controls=0&amp;muted=1?&amp;background=1"></Iframe>
+<Iframe frameBorder="0" allow="autoplay" data-ready="true" data-vimeo-initialized="true" data-desktop="514264128" data-mobile="514264128" muted="" src="https://player.vimeo.com/video/514264128?autoplay=1&amp;loop=1&amp;controls=0&amp;muted=1?&amp;background=1"></Iframe>
     </IframeCont>
     <Overlay>
-      <Title>Waxanimations</Title>
+      <Left>
+      <Title></Title>
       <Text>
         <NMBRS>
+        <Title>Current Employment</Title>
           <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1074.8 323.13">
             <g id="Layer_2" data-name="Layer 2">
               <g id="Layer_1-2" data-name="Layer 1">
@@ -20,21 +22,31 @@ const App = () => {
                 <Path d="M472.84,87.35a2,2,0,0,1,2-2h76.44a66.8,66.8,0,0,1,19.5,2.54A46.12,46.12,0,0,1,585.2,95.1a38,38,0,0,1,10.6,12.83A37.24,37.24,0,0,1,599.61,125q0,10.81-4.66,17.81T580.75,154v.63q12.5,4,19.29,13.78a38.79,38.79,0,0,1,6.78,22.69q0,12.3-4.66,21.41a40.55,40.55,0,0,1-12.72,14.84,47.09,47.09,0,0,1-15.8,7.21,84,84,0,0,1-20.88,2.33H474.84a2,2,0,0,1-2-2Zm71,56.94q6.36,0,10.28-3t3.92-9.12q0-5.92-4-9.22a15.91,15.91,0,0,0-10.39-3.29H518.3a2,2,0,0,0-2,2v20.6a2,2,0,0,0,2,2Zm4,57.87q7.22,0,11.66-4A13.84,13.84,0,0,0,564,187.32q0-7.41-4.77-11a18.64,18.64,0,0,0-11.56-3.61H518.3a2,2,0,0,0-2,2v25.47a2,2,0,0,0,2,2Z"></Path>
                 <Path d="M817.23,240.53a112,112,0,0,1-27.45-3.18,68,68,0,0,1-22-9.64,45.74,45.74,0,0,1-20.39-37,2,2,0,0,1,2-2.13h41.58a2,2,0,0,1,2,1.63,17,17,0,0,0,7.5,11.84q6.68,4.33,17.7,4.34a37.6,37.6,0,0,0,6.67-.63,21.18,21.18,0,0,0,6.26-2.12,13.65,13.65,0,0,0,4.56-3.93,9.51,9.51,0,0,0,1.8-5.83,8.76,8.76,0,0,0-1.91-5.72,15.55,15.55,0,0,0-5.51-4,57.32,57.32,0,0,0-9-3.18q-5.41-1.49-12.4-3.18Q799,175.46,789,172.27a77.51,77.51,0,0,1-18.12-8.37,43.43,43.43,0,0,1-13.36-13.25q-5.19-8.06-5.19-20.14,0-12.93,5-22A41.77,41.77,0,0,1,770.8,93.62a59.06,59.06,0,0,1,19.93-8.38,107.28,107.28,0,0,1,24.17-2.65,98.91,98.91,0,0,1,22.57,2.55,59,59,0,0,1,19.4,8.16,42.76,42.76,0,0,1,13.57,14.42,41.73,41.73,0,0,1,5.23,19,2,2,0,0,1-2,2.13h-40.2a2,2,0,0,1-1.94-1.51,15.38,15.38,0,0,0-5.29-8.35q-4.56-3.7-13.68-3.71-7.41,0-12,3T796,125.84a7.08,7.08,0,0,0,2.33,5.51,19.9,19.9,0,0,0,6.89,3.71,90.34,90.34,0,0,0,11.24,3q6.68,1.38,15.15,3.5a185.21,185.21,0,0,1,18.66,5.62,60.52,60.52,0,0,1,16.33,8.79,41.78,41.78,0,0,1,11.55,13.68q4.34,8.16,4.35,20.24a44.8,44.8,0,0,1-5.2,21.95,47.06,47.06,0,0,1-14.1,15.79,65.36,65.36,0,0,1-20.77,9.65A94.28,94.28,0,0,1,817.23,240.53Z"></Path><Path d="M890.25,196.44h37.34a2,2,0,0,1,2,2v36.49a2,2,0,0,1-2,2H890.25a2,2,0,0,1-2-2V198.44A2,2,0,0,1,890.25,196.44Z"></Path><Path d="M745.56,227.59q-.19-1.76-.33-3.81l-.07-.75,0,0c-.06-.92-.1-1.87-.14-2.87-.14-4-.35-8.34-.63-13.14a90.82,90.82,0,0,0-1.91-14,36,36,0,0,0-6.89-15.16q-5-6.25-14.31-9.86v-.64q12.09-5.5,18.44-14.62t6.37-22.26q0-11.67-4.56-20.15a40.13,40.13,0,0,0-12.41-14,56.78,56.78,0,0,0-18.23-8.27,85.92,85.92,0,0,0-22-2.75H615.61a2,2,0,0,0-2,2V234.93a2,2,0,0,0,2,2h40.3a2,2,0,0,0,2-2V186.14a2,2,0,0,1,2-2h18.57q9.75,0,14.2,4.35t6.57,13.25a64.46,64.46,0,0,1,1.7,10.81c.28,3.81.53,7.42.74,10.81.06.86.12,1.69.18,2.5l.68,9.21a2,2,0,0,0,2,1.86H744a2,2,0,0,0,2-2.13Zm-49.27-79.07q-5.1,4-14.63,4H659.91a2,2,0,0,1-2-2V122.12a2,2,0,0,1,2-2h22.38q9.54,0,14.31,4.45t4.77,11.87Q701.37,144.5,696.29,148.52Z">
                 </Path></g></g></SVG></NMBRS>
-                  <br />
                 <p>A Creative Studio</p>
                 <p>Specialised in Esport &amp; Gaming.</p>
-                  <br />
                 <p>Stockholm</p>
-                <A href="tel:46735392530" target="_blank">+46735392530</A><br/>
-                <A href="mailto: alexander@nmbrs.studio" target="_blank">alexander@nmbrs.studio</A>
-                  <br />
-                  <Text>
-                <A href="https://www.instagram.com/waxanimations/" target="_blank"><img alt="" src="/IG-logo.svg"></img></A>
-                <A href="https://www.linkedin.com/in/alexander-waxin-07a914201" target="_blank">
-                <SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><Path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></SVG>
-                  </A>
-                  </Text>
       </Text>
+      </Left>
+      <Center>
+        <A href="">
+        <Title2>
+          Waxanimations
+        </Title2>
+        </A>
+      </Center>
+      <Right>
+        <Title></Title>
+        <RightCont>
+        <A href="tel:46735392530" target="_blank"><Text2>+46735392530</Text2></A>
+        <A href="mailto: alexander@nmbrs.studio" target="_blank"><Text2>alexander@nmbrs.studio</Text2></A>
+          <Text>
+        <A href="https://www.instagram.com/waxanimations/" target="_blank"><img alt="" src="/IG-logo.svg"></img></A>
+        <A href="https://www.linkedin.com/in/alexander-waxin-07a914201" target="_blank">
+        <SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><Path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></SVG>
+          </A>
+          </Text>
+      </RightCont>
+        </Right>
       </Overlay>
     </Cont>
   )
@@ -53,10 +65,14 @@ const Cont = styled.div`
 `
 
 const Title = styled.p`
+  color: yellow;
+  font-size: 19px;
+  font-weight: 700;
+`
+const Title2 = styled.p`
   color: white;
   font-size: 19px;
   font-weight: 700;
-  padding: 5px;
 `
 const Text = styled.p`
   color: white;
@@ -65,8 +81,15 @@ const Text = styled.p`
   padding: 15px;
   text-decoration: none;
   width: 100%;
+  padding-bottom: 5px;
+  margin-bottom: 5px;
 `
-
+const Text2 = styled.p`
+  color: white;
+  font-size: 14px;
+  font-weight: 700;
+  margin : 4px;
+`
 const Iframe = styled.iframe`
 width: 100%; 
 height: 100%;
@@ -92,14 +115,47 @@ const Overlay = styled.div`
   margin: 0;
   padding: 0;
   display: flex;
+`
+
+const Left = styled.div`
+  display: flex;
   flex-flow: column;
   justify-content: space-between;
+  width: 33.33%;
+`
+
+const Center = styled.div`
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  width: 33.33%;
+  text-align: center;
+  align-items: center;
+`
+
+const Right = styled.div`
+  display: flex;
+  flex-flow: column;
+  justify-content: space-between;
+  width: 33.33%;
+`
+
+const RightCont = styled.div`
+  text-align: right;
+  box-sizing: border-box;
+  padding-left: 35px;
+  padding: 20px;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
 `
 
 const NMBRS = styled.h1`
   text-decoration: none;
   color: white;
   font-size: 14px;
+  padding-bottom: 0;
+  margin-bottom: 0;
   :visited{
     color: white;
   }
@@ -121,3 +177,4 @@ const A = styled.a`
   color: white;
   text-decoration: none;
 `
+
